5d475478a29d3f331b2d0c28fe53c7e9ce3edb45
jaehyuk <ljh@nis.co.kr>
2025-02-03 15:46:39 +0900

    [CHORE] JPA Auditing 적용

--- src/main/java/com/propertyservice/property_service/config/AuditorAwareImpl.java
+++ src/main/java/com/propertyservice/property_service/config/AuditorAwareImpl.java
@@ -1,26 +1,17 @@
-//package com.propertyservice.property_service.config;
-//
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Configurable;
-//import org.springframework.data.domain.AuditorAware;
-//import org.springframework.util.StringUtils;
-//
-//import java.util.Optional;
-//
-//@Configurable
-//public class AuditorAwareImpl implements AuditorAware<String> {
-//
-//    @Autowired
-//    private HttpServletRequest httpServletRequest;
-//
-//    @Override
-//    public Optional<String> getCurrentAuditor() {
-//        String modiedBy = httpServletRequest.getRequestURI();
-//        if(!StringUtils.hasText(modiedBy)){
-//            modiedBy = "unknown";
-//        }
-//        return Optional.of(modiedBy);
-//    }
-//}
+package com.propertyservice.property_service.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+@Configurable
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    @Nullable
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("system");
+    }
+}
---
+++ src/main/java/com/propertyservice/property_service/domain/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.propertyservice.property_service.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
