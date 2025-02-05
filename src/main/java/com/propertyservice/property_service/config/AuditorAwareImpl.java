f983a2fb104567a11cee1a27ef1b15c0fd52cc54
jaehyuk <ljh@nis.co.kr>
2025-02-05 14:13:04 +0900

    [FEAT] Spring Security와 AuditorAware 연동

--- src/main/java/com/propertyservice/property_service/config/AuditorAwareImpl.java
+++ src/main/java/com/propertyservice/property_service/config/AuditorAwareImpl.java
@@ -3,6 +3,9 @@ package com.propertyservice.property_service.config;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -12,6 +15,20 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     @Nullable
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("system");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty(); // 인증되지 않은 경우
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        // UserDetails 타입이면 사용자 이름 반환
+        if (principal instanceof UserDetails) {
+            return Optional.of(((UserDetails) principal).getUsername());
+        }
+
+        // 인증 정보가 문자열로 저장된 경우
+        return Optional.of(principal.toString());
     }
 }
